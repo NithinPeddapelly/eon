4d103198b838b1da48f98f1d01e36304ec14c6d4
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-04-17 17:15:54 +0530

    Create project and seals directories; initialize config.json with default bucket

--- backend/controllers/launch.js
+++ backend/controllers/launch.js
@@ -3,7 +3,17 @@ const path = require("path"); // Importing the path module to create directories
 
 
 async function launchProject() {   
-    console.log("Initializing a new project");
+    const projectPath = path.resolve(process.cwd(), ".eonPaths") // Getting the current working directory or folder path
+    const sealPath = path.join(projectPath, "seals") // Creating a path for the seals directory
+
+    try{
+        await fs.mkdir(projectPath, { recursive: true }); // Creating the seals directory if it doesn't exist
+        await fs.mkdir(sealPath, { recursive: true }); // Creating the seals directory if it doesn't exist
+        await fs.writeFile(
+            path.join(projectPath, "config.json"), JSON.stringify({bucket : "aws-Edit"})); // Creating an empty seals.json file
+    } catch(error) {
+        console.error("Error creating project directory:", error); // Logging the error if the directory creation fails
+    }
 }
 
 module.exports = {launchProject};

9b7236f613dc75033aee45ed995071656e232e8e
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-04-19 23:34:12 +0530

    Configure AWS S3 to different region and update the bucket names accordingly

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -1,8 +1,8 @@
-const AWS = require("aws-sdk");
+const AWS = require("aws-sdk");// Importing the AWS SDK
 
-AWS.config.update({region: "ap-south-1"});
+AWS.config.update({ region: "eu-north-1" });// Updating the region to EU North 1
 
 const s3 = new AWS.S3();// Creating an S3 instance with the specified region
-const S3_BUCKET = "eonofbuckets";// The name of the S3 bucket where files will be uploaded and downloaded
+const S3_BUCKET = "eonfiles";// The name of the S3 bucket where files will be uploaded and downloaded
 
 module.exports = {s3, S3_BUCKET};// Exporting the S3 instance and the bucket name

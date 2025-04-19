79c1d4ad3605c1ff3df124d76e872c745d779e9d
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-04-19 21:42:59 +0530

    changed S3_BUCKET NAME to S3_BUCKET

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -3,6 +3,6 @@ const AWS = require("aws-sdk");
 AWS.config.update({region: "ap-south-1"});
 
 const s3 = new AWS.S3();// Creating an S3 instance with the specified region
-const S3_BUCKET_NAME = "eonofbuckets";// The name of the S3 bucket where files will be uploaded and downloaded
+const S3_BUCKET = "eonofbuckets";// The name of the S3 bucket where files will be uploaded and downloaded
 
-module.exports = {s3, S3_BUCKET_NAME};// Exporting the S3 instance and the bucket name
+module.exports = {s3, S3_BUCKET};// Exporting the S3 instance and the bucket name

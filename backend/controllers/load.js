f1951a4a84cea7e1f68878a5ae51900309e8cbeb
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-04-22 20:23:08 +0530

    Improved CLI feedback across commands using chalk for colorized output

--- backend/controllers/load.js
+++ backend/controllers/load.js
@@ -1,21 +1,22 @@
-const fs = require("fs").promises; // Importing the file system module to read and write files
+const fs = require("fs").promises; // Importing the file system module to read and write files 
 const path = require("path"); // Importing the path module to create directories
-
+const chalk = require('chalk'); // Importing the chalk module to colorize console output
 
 async function loadFile(filepath) { // Function to load a file from a given path
-    const projectPath = path.resolve(process.cwd(), ".eonPaths") // Getting the current working directory or folder path
-    const sealingPath = path.join(projectPath, "sealing") // Creating a path for the sealing directory
+    const projectPath = path.resolve(process.cwd(), ".eonPaths"); // Getting the current working directory or folder path
+    const sealingPath = path.join(projectPath, "sealing"); // Creating a path for the sealing directory
 
-    try{
+    try {
         await fs.mkdir(sealingPath, { recursive: true }); // Creating the sealing file if it doesn't exist
         const fileName = path.basename(filepath); // Getting the file name
         await fs.copyFile(filepath, path.join(sealingPath, fileName)); // Copying the file to the sealing directory
-        console.log(`File ${fileName} added to the sealing area successfully!_2`); // Logging the success message using "`" to avoid escaping the special characters
+        
+        console.log(chalk.green.bold(`\nFile ${fileName} added to the sealing area successfully!_2\n`));
 
+    } catch (error) {
 
-    }catch(error) {
-    console.log("Error sealing file : ",error); // Logging the error if the directory creation fails
+        console.log(chalk.red.bold("\nError sealing file : "), chalk.red(error), '\n');
     }
 }
 
-module.exports = {loadFile};
+module.exports = { loadFile };

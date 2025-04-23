d6ea39e1962bd8426cefd7b10c7006d904e2e3e6
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-04-23 23:33:37 +0530

    Initialized rewind controller with base setup and async utilities

--- backend/controllers/rewind.js
+++ backend/controllers/rewind.js
@@ -1,5 +1,13 @@
+const fs = require("fs").promises; // Importing the file system module to read and write files
+const path = require("path"); // Importing the path module to create directories
+const {promisify} = require("util"); // Importing the promisify function to use fs.readFile in a promise
+const chalk = require('chalk'); // Importing the chalk module to colorize console output
+const readdir = promisify(fs.readdir); // Promisifying the fs.readdir function to use it with async/await
+
+
+
 async function rewindProject() {   
-    console.log("Reverting project to previous state...");
+    
 }
 
 module.exports = {rewindProject};

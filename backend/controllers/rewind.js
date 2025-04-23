b936b169d0bc00f53612104dcf586418715545c8
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-04-24 00:42:21 +0530

    Implemented rewind logic : restored files from seal directory to project root

--- backend/controllers/rewind.js
+++ backend/controllers/rewind.js
@@ -1,4 +1,4 @@
-const fs = require("fs").promises; // Importing the file system module to read and write files
+const fs = require("fs");// Importing the fs module to read and write files
 const path = require("path"); // Importing the path module to create directories
 const {promisify} = require("util"); // Importing the promisify function to use fs.readFile in a promise
 const chalk = require('chalk'); // Importing the chalk module to colorize console output
@@ -7,14 +7,14 @@ const readdir = promisify(fs.readdir); // reads if only the files exist in the d
 const copyFile = promisify(fs.copyFile); // copies a file from one location to another
 
 
-async function rewindProject() {   
+async function rewindProject(sealID) {   
     const projectPath = path.resolve(process.cwd(), ".eonPaths"); // Getting the current working directory
     const sealPath = path.join(projectPath, "seals"); // Creating a path for the seals directory
 
     try{
-        const sealDir = path.jpin(sealPath, sealID); // Creating a path for the seal directory
+        const sealDir = path.join(sealPath, sealID); // Creating a path for the seal directory
         const files = await readdir(sealDir); // Reading the files in the seal directory , if somthing is wrong it will throw an error that means there is no such sealID
-        const parentDir = path.resolve(sealDir, ".."); // Getting the parent directory of the seal directory
+        const parentDir = path.resolve(projectPath, ".."); // Getting the parent directory of the seal directory
 
         for (const file of files) {
             await copyFile(
@@ -22,7 +22,7 @@ async function rewindProject() {
                 path.join(parentDir, file)
                 ); // Copying the files from the seal directory to the parent directory
         }
-        console.log(chalk.green.bold(`\nProject rewinded successfully! \n`));
+        console.log(chalk.green.bold(`\nProject rewinded successfully to ${sealID} seal \n`));
 
     } catch(error){
         console.error(chalk.red.bold("\nError rewinding project : "), chalk.red(error), '\n');

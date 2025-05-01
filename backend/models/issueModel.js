0f63e73c93ede75b9edab98c5ddfbde30cdaa8e3
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-05-01 19:32:54 +0530

    Defined Issue schema with fields for title, description, status, and associated project; created and exported the Issue model

---
+++ backend/models/issueModel.js
@@ -0,0 +1,29 @@
+import { required } from "yargs";
+
+const mongoose = require("mongoose");// Importing the mongoose module to interact with MongoDB
+const {Schema} = mongoose; // Importing the Schema class from mongoose
+
+const IssueSchema = new Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    status:{
+        type: String,
+        required: true,
+        enum: ["open", "in-progress", "closed"], // The status can only be one of these values
+        default: "open", // The default status is "open"
+    },
+    Project:{
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required: true,
+    },
+ });
+
+const Issue = mongoose.model("Issue", IssueSchema); // Creating a model from the schema
+export default Issue; // Exporting the Issue model as the default export of this module
